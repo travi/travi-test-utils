b59f54aeab2e8f91ed73902cbdfd4eaa05e79902
Matt Travi <programmer@travi.org>
2014-06-08 12:23:03 -0500

    Revert "uncomment failures for amplifyStub. closes #6"
    This reverts commit a7f54348dd52f8c71294ec22d4d5ba074f5fbfc2 since it
    still causes issues in some of the consuming repos.

--- stubs/amplifyStub.js
+++ stubs/amplifyStub.js
@@ -1,11 +1,11 @@
 var amplify = {
     publish: function (eventName, data) {
-        fail('you should be stubbing publish');
+//        fail('you should be stubbing publish');
     },
     subscribe: function (eventName, callback) {
-        fail('you should be stubbing subscribe');
+//        fail('you should be stubbing subscribe');
     },
     unsubscribe: function (eventName, callback) {
-        fail('you should be stubbing unsubscribe');
+//        fail('you should be stubbing unsubscribe');
     }
 };
